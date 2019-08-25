c40dfcdfddf3a5f8e4948c783d02f71b02743e89
chrisdoberman <chris.herb@gmail.com>
2019-08-25 15:19:55 -0400

    testing commit across two repos/branches

--- src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
+++ src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeerOrderServiceApplication {
-
+    //testing git
     public static void main(String[] args) {
         SpringApplication.run(BeerOrderServiceApplication.class, args);
     }
